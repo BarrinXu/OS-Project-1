f9d1e0cd6276ddc6c43de95c1ea453074cf95fb9
Barrin <barrin@qq.com>
2021-05-05 16:44:39 +0800

    -

--- kernel/common/answer_locks.h
+++ kernel/common/answer_locks.h
@@ -10,21 +10,36 @@ int lock_init(struct lock *lock){
     /* Your code here */
     if(nlock >= MAXLOCKS) BUG("Max lock count reached.");
     locks[nlock++] = lock;
+    lock->locked=0;
     return 0;
 }
 
 void acquire(struct lock *lock){
     /* Your code here */
+    while (try_acquire(lock)!=0);
+
 }
 
 // Try to acquire the lock once
 // Return 0 if succeed, -1 if failed.
 int try_acquire(struct lock *lock){
     /* Your code here */
-    return -1;
+    int id=__sync_lock_test_and_set(&(lock->locked),1);
+    if(id!=0)
+        return -1;
+    __sync_lock_test_and_set(&(lock->cpuid),cpuid());
+    return 0;
 }
 
 void release(struct lock* lock){
+    if(cpuid()!=lock->cpuid)
+        BUG("Try to release other's lock!");
+    if(!is_locked(lock))
+        BUG("Try to release an already unlocked lock!");
+    __sync_lock_release(&(lock->locked));
+    /*int id=__sync_lock_test_and_set(&(lock->locked),0);
+    if(id!=1)
+        BUG("Try to release an already unlocked lock!");*/
     /* Your code here */
 }
 
@@ -35,7 +50,7 @@ int is_locked(struct lock* lock){
 // private for spin lock
 int holding_lock(struct lock* lock){
     /* Your code here */
-    return -1;
+    return lock->locked&&cpuid()==lock->cpuid;
 }
 
 #endif  // ACMOS_SPR21_ANSWER_LOCKS_H
--- kernel/common/answer_printk.h
+++ kernel/common/answer_printk.h
@@ -4,12 +4,17 @@
 
 #ifndef ACMOS_SPR21_ANSWER_PRINTK_H
 #define ACMOS_SPR21_ANSWER_PRINTK_H
-
 static void printk_write_string(const char *str) {
     // Homework 1: YOUR CODE HERE
     // this function print string by the const char pointer
     // I think 3 lines of codes are enough, do you think so?
     // It's easy for you, right?
+
+    while(*str != '\0'){
+        uart_putc(*str);
+        str++;
+    }
+
 }
 
 
@@ -20,6 +25,33 @@ static void printk_write_num(int base, unsigned long long n, int neg) {
     // you do not need to print prefix like "0x", "0"...
     // Remember the most significant digit is printed first.
     // It's easy for you, right?
+    int k=33;
+    char s[34];
+    s[k]='\0';
+    if(neg==1)
+        s[--k]='-';
+    if(base==2) {
+        while(n){
+            s[--k]='0'+n%2;
+            n/=2;
+        }
+    }
+    else if(base==10) {
+        while(n){
+            s[--k]='0'+n%10;
+            n/=10;
+        }
+    }
+    else if(base==16){
+        while(n){
+            if(n%16<10)
+                s[--k]='0'+n%16;
+            else
+                s[--k]='a'+n%16-10;
+            n/=16;
+        }
+    }
+    printk_write_string(&s[k]);
 }
 
 #endif  // ACMOS_SPR21_ANSWER_PRINTK_H
